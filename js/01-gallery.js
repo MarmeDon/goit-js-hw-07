514410ba3deb1dbf4be6e0a4fe99eb9ff8aa5572
Pavlo Shymko <shymko.p@gmail.com>
2022-08-02 22:31:42 +0300

    add code with solutions

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,57 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const refs = {
+   gallery: document.querySelector('.gallery'),
+}
+
+function createGalleryItemsMarkUp( { description, original, preview } ) {
+   return `<div class="gallery__item">
+   <a class="gallery__link" href="${original}">
+   <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+      />
+   </a>
+</div>`
+}
+
+const makeGalleryMarkUp = galleryItems.map(createGalleryItemsMarkUp).join('');
+
+refs.gallery.innerHTML = makeGalleryMarkUp;
+
+// function onClick(event) {
+//    event.preventDefault();
+//    basicLightbox.create(`
+// 		<img src="${event.target.dataset.source}">
+// 	`).show();
+// }
+
+refs.gallery.addEventListener('click', onClick)
+
+// Version with Escape
+
+function onClick(event) {
+   event.preventDefault();
+	const source = event.target.dataset.source;
+   onOpenModal(source);
+}
+
+function onOpenModal(source) {
+   const instance = basicLightbox.create(`
+		<img src="${source}">
+	`)
+   instance.show();
+
+   function onCloseModal(event) {
+   if (event.code === 'Escape') {
+      instance.close();
+   }
+}
+   window.addEventListener('keydown', onCloseModal);
+}
+
+
+
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,23 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const refs = {
+   gallery: document.querySelector('.gallery'),
+}
+
+function createGalleryItemsMarkUp( { description, original, preview } ) {
+   return `<a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+</a>`
+}
+
+const makeGalleryMarkUp = galleryItems.map(createGalleryItemsMarkUp).join('');
+
+refs.gallery.innerHTML = makeGalleryMarkUp;
+
+let gallery = new SimpleLightbox('.gallery a', {
+   captionsData: 'alt',
+   captionDelay: 250
+});
+gallery.on('show.simplelightbox');
